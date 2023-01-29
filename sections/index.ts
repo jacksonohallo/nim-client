ec332f2064a8534d38b4975c759269506b44fdca
jacksonohallo <jacksonohallo@gmail.com>
2023-01-29 11:55:35 +0300

    remove projects

--- sections/index.ts
+++ sections/index.ts
@@ -1,7 +1,6 @@
 import About from "./About/About";
 import Contact from "./Contact/Contact";
-import Experience from "./Experience/Experience";
 import Hero from "./Hero/Hero";
-import Projects from "./Projects/Projects";
+
 
 export { About, Contact, Experience, Hero, Projects };
